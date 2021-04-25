ad90857a1e48816a7e4bd6b0d339320941a1a8da
w693847022 <55230165+w693847022@users.noreply.github.com>
2021-04-25 19:26:33 +0800

    wrs 2021/19/26 19:26

--- headerfile/Queue_w.h
+++ headerfile/Queue_w.h
@@ -2,10 +2,27 @@
 typedef void* Queue_t[2];
 class Queue_w{
     public:
-    inline Queue_t* Next(){ return &(*(&queue))[2];}
-    inline Queue_t* Prev(){return (Queue_t*)queue[1];}
-    inline Queue_t* NextPrev(){return (Queue_t*)((Queue_t*)queue[2])[1];}
-    inline Queue_t* PrevNext(){return (Queue_t*)((Queue_t*)queue[1])[2];}
+    inline Queue_w* GetNext(){return (Queue_w*)&((*(&queue))[2]);}//通过 引用再解引用使其变成左值
+    inline Queue_w* GetPrev(){return (Queue_w*)&((*(&queue))[1]);}
+    inline Queue_w* GetNextPrev(){return (Queue_w*)&((* ( &(*(Queue_w**)GetNext()) ) )[1]);}
+    inline Queue_w* GetPrevNext(){return (Queue_w*)&((* ( &(*(Queue_w**)GetPrev()) ) )[2]);}
+    void SetNext(Queue_w* q)
+    {
+        *(Queue_w**)&((*(&queue))[2]) =  q;
+    }
+    void SetPrve(Queue_w* q)
+    {
+        *(Queue_w**)&((*(&queue))[1]) = q;
+    }
+    void SetNextPrve(Queue_w * q)
+    {
+       *(Queue_w**)&(*(&(*(Queue_w**)GetNext()))[1]) =  q;
+    }
+    void SetPrveNext(Queue_w *q)
+    {
+        *(Queue_w**)&(*(&(*(Queue_w**)GetPrev()))[2]) = q;
+    }
+    
     Queue_t* queue;//[1] prev [2] next
 };
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include<stdafx.h>
-#include <typeinfo>
 using namespace std;
-typedef void* w[2];
+
+
 void main()
 {
-    Queue_w* q1;
-    // Queue_w* q2;
-    // (*(Queue_t**)q1->Next()) = q2->queue;
-
-    
-    getchar();
+    cout<<"play main"<<endl;
 }
